b00c43a7f25f940915cc58e76d500a9496566fac
Dingjun Yue <dingjun.yue@gmail.com>
2020-09-13 22:34:33 +0800

    Course Credits working
    Associated Student, Course and CourseCredit class using SQLAlchemy relationships.
    Next steps:
    	1. Apply the same relationship to other similar tables
    	2. Study on back_populate and other properties

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from flask_mail import Mail, Message
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'sms.db')
 app.config['JWT_SECRET_KEY'] = 'super-secret' # CHANGE THIS LATER
 app.config
 
@@ -16,43 +16,40 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
 
-class User(Base):
-...     __tablename__ = 'users'
-...
-...     id = Column(Integer, primary_key=True)
-...     name = Column(String)
-...     fullname = Column(String)
-...     nickname = Column(String)
-...
-...     def __repr__(self):
-...        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-...                             self.name, self.fullname, self.nickname)
 
-class Students(db.Model):
+class Student(db.Model):
     __tablename__ = 'students'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
+    student_credits = db.relationship("CourseCredit", back_populates="student")
+
     def __repr__(self):
         return "<Student(name='%s')>" % (self.name)
 
 
-class Courses(db.Model):
+class Course(db.Model):
     __tablename__ = "courses"
 
     id = Column(Integer, primary_key=True)
     course_name = Column(String)
 
+    course_credits = db.relationship("CourseCredit", back_populates="course")
+
     def __repr__(self):
             return "<Course(course name='%s')>" % (self.course_name)
 
 
-class CourseCredits(db.Model):
+class CourseCredit(db.Model):
     __tablename__ = "courseCredits"
 
-    id = Column(Integer, primary_key=True)
-    
+    student_id = Column(Integer, ForeignKey('students.id'), primary_key=True)
+    course_id = Column(Integer, ForeignKey('courses.id'), primary_key=True)
+    number_of_classes = Column(Integer)
+
+    student = db.relationship("Student", back_populates="student_credits")
+    course = db.relationship("Course", back_populates="course_credits")
 
 
 @app.cli.command('db_create')
@@ -69,37 +66,36 @@ def db_drop():
 
 @app.cli.command('db_seed')
 def db_seed():
-    mercury = Planet(planet_name='Mercury',
-                    planet_type='Class D',
-                    home_star='Sol',
-                    mass=3.258e23,
-                    radius=1516,
-                    distance=35.98e6)
 
-    venus = Planet(planet_name='Venus',
-                    planet_type='Class K',
-                    home_star='Sol',
-                    mass=4.867e24,
-                    radius=3760,
-                    distance=67.24e6)
+    student = Student(name="岳鼎钧")
+    english = Course(course_name='English')
+    courseNo = CourseCredit(number_of_classes=100)
+    courseNo.student = student
+    english.course_credits.append(courseNo)
 
-    earth = Planet(planet_name='Earth',
-                    planet_type='Class M',
-                    home_star='Sol',
-                    mass=5.972e24,
-                    radius=3959,
-                    distance=92.96e6)
+
+
+    # venus = Planet(planet_name='Venus',
+    #                 planet_type='Class K',
+    #                 home_star='Sol',
+    #                 mass=4.867e24,
+    #                 radius=3760,
+    #                 distance=67.24e6)
+
+    # earth = Planet(planet_name='Earth',
+    #                 planet_type='Class M',
+    #                 home_star='Sol',
+    #                 mass=5.972e24,
+    #                 radius=3959,
+    #                 distance=92.96e6)
     
-    db.session.add(mercury)
-    db.session.add(venus)
-    db.session.add(earth)
+    # db.session.add(english)
 
-    test_user = User(first_name='William',
-                    last_name='Herschel',
-                    email='test@test.com',
-                    password='1234')
+    # SQLAlchemy has cascading property, so I just need to add one of the objects to the session.
+    # https://docs.sqlalchemy.org/en/13/orm/cascades.html#unitofwork-cascades
+    db.session.add(student)
+    # db.session.add(courseNo)
 
-    db.session.add(test_user)
     db.session.commit()
     print('Database seeded!')
 
@@ -178,42 +174,42 @@ def login():
         return jsonify(message='Bad email or password!'), 401
 
 
-# database models
-class User(db.Model):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String, unique=True)
-    password = Column(String)
+# # database models
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True)
+#     first_name = Column(String)
+#     last_name = Column(String)
+#     email = Column(String, unique=True)
+#     password = Column(String)
 
 
-class Planet(db.Model):
-    __tablename__ = 'planets'
-    planet_id = Column(Integer, primary_key=True)
-    planet_name = Column(String)
-    planet_type = Column(String)
-    home_star = Column(String)
-    mass = Column(Float)
-    radius = Column(Float)
-    distance = Column(Float)
+# class Planet(db.Model):
+#     __tablename__ = 'planets'
+#     planet_id = Column(Integer, primary_key=True)
+#     planet_name = Column(String)
+#     planet_type = Column(String)
+#     home_star = Column(String)
+#     mass = Column(Float)
+#     radius = Column(Float)
+#     distance = Column(Float)
 
 
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+# class UserSchema(ma.Schema):
+#     class Meta:
+#         fields = ('id', 'first_name', 'last_name', 'email', 'password')
 
 
-class PlanetSchema(ma.Schema):
-    class Meta:
-        fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
+# class PlanetSchema(ma.Schema):
+#     class Meta:
+#         fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
 
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+# user_schema = UserSchema()
+# users_schema = UserSchema(many=True)
 
-planet_schema = PlanetSchema()
-planets_schema = PlanetSchema(many=True)
+# planet_schema = PlanetSchema()
+# planets_schema = PlanetSchema(many=True)
 
 
 if __name__ == '__main__':
