f5313cc29708497f2d56984cbdeff265f1d9f235
Dingjun Yue <dingjun.yue@gmail.com>
2020-09-23 17:41:49 +0800

    updated db relationships

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from flask_mail import Mail, Message
@@ -16,15 +16,13 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
 
-
+# All relationships are configured on the "many" side of a one-to-many relationship.
 class Student(db.Model):
     __tablename__ = 'students'
 
     id = Column(Integer, primary_key=True)
+    deleted = Column(Boolean)
     name = Column(String)
-
-    student_credits = db.relationship("CourseCredit", back_populates="student")
-    # partents = db.relationship("ParentHood", back_populates="students")
 
     def __repr__(self):
         return "<Student(name='%s')>" % (self.name)
@@ -34,6 +32,7 @@ class Teacher(db.Model):
     __tablename__ = "teachers"
 
     id = Column(Integer, primary_key=True)
+    deleted = Column(Boolean)
     name = Column(String)
 
 
@@ -41,6 +40,7 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
+    deleted = Column(Boolean)
     name = Column(String)
 
 
@@ -48,13 +48,11 @@ class Course(db.Model):
     __tablename__ = "courses"
 
     id = Column(Integer, primary_key=True)
+    deleted = Column(Boolean)
     course_name = Column(String)
 
-    course_credits = db.relationship("CourseCredit", back_populates="course")
-    class_sessions = db.relationship("ClassSession", back_populates="course")
-
     def __repr__(self):
-            return "<Course(course name='%s')>" % (self.course_name)
+        return "<Course(course name='%s')>" % (self.course_name)
 
 
 class CourseCredit(db.Model):
@@ -62,10 +60,12 @@ class CourseCredit(db.Model):
 
     student_id = Column(Integer, ForeignKey('students.id'), primary_key=True)
     course_id = Column(Integer, ForeignKey('courses.id'), primary_key=True)
-    number_of_classes = Column(Integer)
+    deleted = Column(Boolean)
+    credit = Column(Integer)
 
-    student = db.relationship("Student", back_populates="student_credits")
-    course = db.relationship("Course", back_populates="course_credits")
+    # Relationships
+    student = db.relationship("Student", backref="student_credits")
+    course = db.relationship("Course", backref="course_credits")
 
 
 class ClassSession(db.Model):
@@ -73,16 +73,23 @@ class ClassSession(db.Model):
 
     id = Column(Integer, primary_key=True)
     course_id = Column(Integer, ForeignKey("courses.id"))
+    deleted = Column(Boolean)
     info = Column(String)
 
-    course = db.relationship("Course", back_populates="class_sessions")
+    # Relationships
+    course = db.relationship("Course", backref="course_sessions")
 
-# Need to add relationships to the following classes --------------------------------------------------------------
+
 class ParentHood(db.Model):
     __tablename__ = "parenthoods"
 
     student_id = Column(Integer, ForeignKey("students.id"), primary_key=True)
     parent_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    deleted = Column(Boolean)
+
+    # Relationships
+    student = db.relationship("Student", backref="parents")
+    parent = db.relationship("User", backref="students")
 
 
 class Teaching(db.Model):
@@ -90,14 +97,25 @@ class Teaching(db.Model):
 
     session_id = Column(Integer, ForeignKey("classSessions.id"), primary_key=True)
     teacher_id = Column(Integer, ForeignKey("teachers.id"), primary_key=True)
+    deleted = Column(Boolean)
+
+    # Relationships
+    class_session = db.relationship("ClassSession", backref="session_teachings")
+    teacher = db.relationship("Teacher", backref="teacher_teachings")
 
 
 class TakingClass(db.Model):
     __tablename__ = "takingClasses"
 
     session_id = Column(Integer, ForeignKey("classSessions.id"), primary_key=True)
-    teacher_id = Column(Integer, ForeignKey("teachers.id"), primary_key=True)
+    student_id = Column(Integer, ForeignKey("students.id"), primary_key=True)
+    deleted = Column(Boolean)
     comments = Column(String)
+
+    # Relationships
+    class_session = db.relationship("ClassSession", backref="session_takings")
+    student = db.relationship("Student", backref="student_takings")
+
 
 
 
@@ -116,7 +134,7 @@ def db_drop():
 @app.cli.command('db_seed')
 def db_seed():
 
-    student = Student(name="abc")
+    student = Student(name="abc", deleted=False)
     english = Course(course_name='English')
     courseNo = CourseCredit(number_of_classes=100)
     # courseNo.student = student
@@ -124,107 +142,129 @@ def db_seed():
     student.student_credits.append(courseNo)
 
     session1 = ClassSession(info="what's up")
-    english.class_sessions.append(session1)
+    english.course_sessions.append(session1)
 
+    teaching = Teaching()
+    teacher = Teacher(name="teacher1")
+    teacher.teacher_teachings.append(teaching)
+    session1.session_teachings.append(teaching)
 
+    taking_class = TakingClass()
+    session1.session_takings.append(taking_class)
+    student.student_takings.append(taking_class)
 
-    # venus = Planet(planet_name='Venus',
-    #                 planet_type='Class K',
-    #                 home_star='Sol',
-    #                 mass=4.867e24,
-    #                 radius=3760,
-    #                 distance=67.24e6)
-
-    # earth = Planet(planet_name='Earth',
-    #                 planet_type='Class M',
-    #                 home_star='Sol',
-    #                 mass=5.972e24,
-    #                 radius=3959,
-    #                 distance=92.96e6)
-    
-    db.session.add(english)
+    parent_hood = ParentHood()
+    user = User(name="user")
+    student.parents.append(parent_hood)
+    user.students.append(parent_hood)
 
     # SQLAlchemy has cascading property, so I just need to add one of the objects to the session.
     # https://docs.sqlalchemy.org/en/13/orm/cascades.html#unitofwork-cascades
     # db.session.add(student)
     # db.session.add(courseNo)
+    db.session.add(english)
+    db.session.add(user)
 
     db.session.commit()
     print('Database seeded!')
 
+#---------------API Section--------------------------------------------------------------------------
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# All APIs communicate using JSON
 
+@app.route('/addStudent', methods=['POST'])
+def addStudent():
+    name = request.json['name']
+    deleted = request.json['deleted']
 
-@app.route('/super_simple')
-def super_simple():
-    return jsonify(message='Hello from the Planetary API.'), 200
-
-
-@app.route('/not_found')
-def not_found():
-    return jsonify(message='That resource was not found'), 404
-
-
-@app.route('/parameters')
-def parameters():
-    name = request.args.get('name')
-    age = int(request.args.get('age'))
-    if age < 18:
-        return jsonify(message='Sorry ' + name + ', you are not old enough.'), 401
+    already_existed = User.query.filter_by(name=name).first()
+    if already_existed:
+        return jsonify(message='The student already exists'), 409
     else:
-        return jsonify(message='Welcome ' + name + ', you are old enough!')
-
-
-@app.route('/url_variables/<string:name>/<int:age>')
-def url_variables(name: str, age: int):
-    if age < 18:
-        return jsonify(message='Sorry ' + name + ', you are not old enough.'), 401
-    else:
-        return jsonify(message='Welcome ' + name + ', you are old enough!')
-
-
-@app.route('/planets', methods=['GET'])
-def planets():
-    planets_list = Planet.query.all()
-    result = planets_schema.dump(planets_list)
-    return jsonify(result)
-
-
-@app.route('/register', methods=['POST'])
-def register():
-    email = request.form['email']
-    test = User.query.filter_by(email=email).first()
-    print(email)
-    if test:
-        return jsonify(message='That email already exists'), 409
-    else:
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        password = request.form['password']
-        user = User(first_name=first_name, last_name=last_name, email=email, password=password)
-        db.session.add(user)
+        student = Student(name=name, deleted=deleted)
+        db.session.add(student)
         db.session.commit()
-        return jsonify(message='User created successfully.'), 201
+        return jsonify(message="Student created successfully"), 201
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    if request.is_json:
-        email = request.json['email']
-        password = request.json['password']
-    else:
-        email = request.form['email']
-        password = request.form['password']
 
-    test = User.query.filter_by(email=email, password=password).first()
-    if test:
-        access_token = create_access_token(identity=email)
-        return jsonify(message='Login succeeded!', access_token=access_token)
-    else:
-        return jsonify(message='Bad email or password!'), 401
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     if request.is_json:
+#         email = request.json['email']
+#         password = request.json['password']
+#     else:
+#         email = request.form['email']
+#         password = request.form['password']
+
+#     test = User.query.filter_by(email=email, password=password).first()
+#     if test:
+#         access_token = create_access_token(identity=email)
+#         return jsonify(message='Login succeeded!', access_token=access_token)
+#     else:
+#         return jsonify(message='Bad email or password!'), 401
+
+
+# @app.route('/addStudent', methods=['POST'])
+# def addStudent():
+#     email = request.form['email']
+#     test = User.query.filter_by(email=email).first()
+#     print(email)
+#     if test:
+#         return jsonify(message='That email already exists'), 409
+#     else:
+#         first_name = request.form['first_name']
+#         last_name = request.form['last_name']
+#         password = request.form['password']
+#         user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+#         db.session.add(user)
+#         db.session.commit()
+#         return jsonify(message='User created successfully.'), 201
+
+
+
+
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+
+
+# @app.route('/super_simple')
+# def super_simple():
+#     return jsonify(message='Hello from the Planetary API.'), 200
+
+
+# @app.route('/not_found')
+# def not_found():
+#     return jsonify(message='That resource was not found'), 404
+
+
+# @app.route('/parameters')
+# def parameters():
+#     name = request.args.get('name')
+#     age = int(request.args.get('age'))
+#     if age < 18:
+#         return jsonify(message='Sorry ' + name + ', you are not old enough.'), 401
+#     else:
+#         return jsonify(message='Welcome ' + name + ', you are old enough!')
+
+
+# @app.route('/url_variables/<string:name>/<int:age>')
+# def url_variables(name: str, age: int):
+#     if age < 18:
+#         return jsonify(message='Sorry ' + name + ', you are not old enough.'), 401
+#     else:
+#         return jsonify(message='Welcome ' + name + ', you are old enough!')
+
+
+# @app.route('/planets', methods=['GET'])
+# def planets():
+#     planets_list = Planet.query.all()
+#     result = planets_schema.dump(planets_list)
+#     return jsonify(result)
 
 
 # # database models
