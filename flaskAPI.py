2804cfd2ae0526ba81477827ef967e9da3f15c92
Dingjun Yue <dingjun.yue@gmail.com>
2020-10-11 22:38:15 +0800

    Restructured app

---
+++ flaskAPI.py
@@ -0,0 +1,12 @@
+## This is the top level of the program
+
+from app import create_app, db, cli
+from app.models import User, Post
+
+app = create_app()
+cli.register(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
