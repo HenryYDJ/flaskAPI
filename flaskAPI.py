0f497ada363c113393e1c08f5fadf22aa0af6632
Dingjun Yue <dingjun.yue@gmail.com>
2020-10-13 22:39:00 +0800

    doing minor changes

--- flaskAPI.py
+++ flaskAPI.py
@@ -1,12 +1,16 @@
 ## This is the top level of the program
 
-from app import create_app, db, cli
-from app.models import User, Post
+from app import create_app
+# from app.models import User, Post
 
 app = create_app()
-cli.register(app)
+# cli.register(app)
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
