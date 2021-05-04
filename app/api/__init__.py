0858b9954cd9a3f852560d8f4b0bb726822a3218
Yue Ding Jun <dingjun.yue@gmail.com>
2021-05-04 18:45:29 +0800

    Add parent API implemented

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bluePrint = Blueprint('api', __name__)
 
-from app.api import course, student, teacher, user, auth, wechat, util
+from app.api import course, student, teacher, user, auth, wechat, util, parent
